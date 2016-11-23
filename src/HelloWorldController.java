cb04a0b18207d2146b1e0f0ade824f1573224bd4
sonali <sonali@gmail.com>
2016-11-23 18:36:27 +0530

    first commit

---
+++ src/HelloWorldController.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@ Controller
+public class HelloWorldController {
+
+	 @RequestMapping("/hellopage") 
+	 public ModelAndView helloworld()
+	 {
+		 String message = "HELLO SPRING MVC HOW R U";  
+		 return new ModelAndView("hellopage","message",message);
+	 }
+}
+ 
