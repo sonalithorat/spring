3aeea15b950bb8faf5b9cd8c63d61e51c536f763
sonali <sonali@gmail.com>
2016-11-24 14:39:14 +0530

    first commit

--- src/HelloWorldController.java
+++ src/HelloWorldController.java
@@ -2,7 +2,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@ Controller
+@ Controller //anotation
 public class HelloWorldController {
 
 	 @RequestMapping("/hellopage") 
